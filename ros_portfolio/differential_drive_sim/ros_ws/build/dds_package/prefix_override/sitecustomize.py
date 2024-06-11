c37171b89fba93361546f8c851fb1d51858e78f2
I-Corinthian <simonjoshua312@gmail.com>
2024-06-11 23:35:15 +0530

    differential_drive_sim built

---
+++ ros_portfolio/differential_drive_sim/ros_ws/build/dds_package/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/icorinthian/Documents/ros/git/ROS_Portfolio/ros_portfolio/differential_drive_sim/ros_ws/install/dds_package'
---
+++ ros_portfolio/differential_drive_sim/ros_ws/build/dds_package/share/dds_package/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/icorinthian/Documents/ros/git/ROS_Portfolio/ros_portfolio/differential_drive_sim/ros_ws/build/dds_package"
