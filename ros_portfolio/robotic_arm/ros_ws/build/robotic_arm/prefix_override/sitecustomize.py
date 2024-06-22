ce39c193a1481ed6a7209ad60b56bfb497a94adf
I-Corinthian <simonjoshua312@gmail.com>
2024-06-22 22:32:18 +0530

    project 4 robotic arm added

---
+++ ros_portfolio/robotic_arm/ros_ws/build/robotic_arm/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/icorinthian/Documents/ros/git/ROS_Portfolio/ros_portfolio/robotic_arm/ros_ws/install/robotic_arm'
---
+++ ros_portfolio/robotic_arm/ros_ws/build/robotic_arm/share/robotic_arm/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/icorinthian/Documents/ros/git/ROS_Portfolio/ros_portfolio/robotic_arm/ros_ws/build/robotic_arm"
